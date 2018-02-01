59feb573c5fa3eb2f8feb25303a28747f355383a
Keegan Griffee <griffkee@cs.washington.edu>
2018-02-01 14:04:44 -0800

    Created a 2nd app version

--- host/cl_pcie_perf/app2.cpp
+++ host/cl_pcie_perf/app2.cpp
@@ -20,14 +20,9 @@ using namespace std;
 using namespace std::this_thread;
 using namespace std::chrono;
 
-#define START_ADDR_REG_ADDR 0x500
-#define BURST_LEN_REG_ADDR 0x504
-#define WRITE_VAL_REG_ADDR 0x508
-#define RHASH_REG_ADDR 0x50c
-#define RW_EN_REG_ADDR 0x510
-#define RW_DONE_REG_ADDR 0x514
-#define RD_CLK_COUNT_REG_ADDR 0x518
-#define WR_CLK_COUNT_REG_ADDR 0x51c
+#define SETTING_REG_ADDR 0x500
+#define WRITE_FINISHED_REG_ADDR 0x504
+
 #define BUF_SIZE (1ULL << 34)
 
 const int SLOT_ID = 0;
@@ -38,7 +33,7 @@ const int CLK_FREQ = 250000000;
 int main(int argc, char ** argv)
 {		
 	auto fabricManager = new FabricManager();
-	//auto pciHandler = new PCIHandler();
+	auto pciHandler = new PCIHandler();
 	auto dmaController = new DMAController();
 	auto stopwatch = new Stopwatch();
 	fpga_mgmt_image_info_t* info = 0;
@@ -54,26 +49,28 @@ int main(int argc, char ** argv)
 		printf("Vendor ID: 0x%x\r\n", info->spec.map[FPGA_APP_PF].vendor_id);
 		printf("Device ID: 0x%x\r\n", info->spec.map[FPGA_APP_PF].device_id);
 
-		/*
 		// attach pciHandler.
 		pciHandler->attach(SLOT_ID, FPGA_APP_PF, APP_PF_BAR0);
-		*/
 
 		// init dmaController.
 		dmaController->init(SLOT_ID);
 
-		/*
-		// set start_addr
-		pciHandler->poke(START_ADDR_REG_ADDR, 0);
-		uint32_t start_addr_read = pciHandler->peek(START_ADDR_REG_ADDR);
-		if (start_addr_read != 0)
+		// set setting register
+		pciHandler->poke(SETTING_REG_ADDR, 1);
+		uint32_t start_addr_read = pciHandler->peek(SETTING_REG_ADDR);
+		if (start_addr_read != 1)
 		{
-			throw runtime_error("failed to write start_addr\r\n");
+			throw runtime_error("failed to write setting\r\n");
 		}
-		*/
+
+
+		uint32_t write_finished_read = pciHandler->peek(WRITE_FINISHED_REG_ADDR);
+		if (write_finished_read != 0)
+		{
+			throw runtime_error("failed to read write_finished_reg\r\n");
+		}
 
 		// real test
-		uint32_t ocl_read = 0;	
 		size_t burst_len = 1;
 		for (int i = 0; i < 29; i++)
 		{
@@ -84,10 +81,14 @@ int main(int argc, char ** argv)
 
 			for (int j = 0; j < num_trial; j++)
 			{
-				// init random char buffer
+				// Have final bit be a 0 char buffer
 				for (size_t k = 0; k < burst_len*BYTE_PER_BURST; k++)
 				{
-					buf1[k] = (char) (97 + (rand() % 26));
+					if (k == burst_len*BYTE_PER_BURST - 1) {
+						buf1[k] = (char) 0; 	
+					} else {
+						buf1[k] = (char) 1;
+					}
 				}
 				
 				// DMA write
@@ -95,39 +96,27 @@ int main(int argc, char ** argv)
 				dmaController->write(buf1, burst_len*BYTE_PER_BURST, 0, 0);
 				dma_write_latency[j] = stopwatch->stop();
 		
-
+				/*
 				// DMA read
 				stopwatch->start();
 				dmaController->read(buf2, burst_len*BYTE_PER_BURST, 0, 0);	
 				dma_read_latency[j] = stopwatch->stop();
-				
+				*/
 			}
 
-			double dma_read_avg = MathHelper::average(dma_read_latency, num_trial);
-			double dma_read_stdev = MathHelper::stdev(dma_read_latency, num_trial);
+			uint32_t write_finished_read = pciHandler->peek(WRITE_FINISHED_REG_ADDR);
+			if (write_finished_read != 1)
+			{
+				throw runtime_error("failed to verify that the pattern was correctly matched\r\n");
+			}
+
+			//double dma_read_avg = MathHelper::average(dma_read_latency, num_trial);
+			//double dma_read_stdev = MathHelper::stdev(dma_read_latency, num_trial);
 			double dma_write_avg = MathHelper::average(dma_write_latency, num_trial);
 			double dma_write_stdev = MathHelper::stdev(dma_write_latency, num_trial);
 				
-			double cl_read_avg = MathHelper::average(cl_read_latency, num_trial);
-			double cl_read_stdev = MathHelper::stdev(cl_read_latency, num_trial);
-			double cl_write_avg = MathHelper::average(cl_write_latency, num_trial);
-			double cl_write_stdev = MathHelper::stdev(cl_write_latency, num_trial);
-			
-			double cl_read_clk_count_avg = MathHelper::average(cl_read_clk_count, num_trial);
-			double cl_read_clk_count_stdev = MathHelper::stdev(cl_read_clk_count, num_trial);
-			
-			double cl_write_clk_count_avg = MathHelper::average(cl_write_clk_count, num_trial);
-			double cl_write_clk_count_stdev = MathHelper::stdev(cl_write_clk_count, num_trial);
-	
 			printf("dma,read,%lu,%f,%f\r\n", burst_len, dma_read_avg*1000, dma_read_stdev*1000);
 			printf("dma,write,%lu,%f,%f\r\n", burst_len, dma_write_avg*1000, dma_write_stdev*1000);
-			printf("cl,read,%lu,%f,%f\r\n", burst_len, cl_read_avg*1000, cl_read_stdev*1000);
-			printf("cl,write,%lu,%f,%f\r\n", burst_len, cl_write_avg*1000, cl_write_stdev*1000);
-			printf("internal_clk,read,%lu,%f,%f\r\n",burst_len, cl_read_clk_count_avg/CLK_FREQ*1000,
-				cl_read_clk_count_stdev/CLK_FREQ*1000);
-			printf("internal_clk,write,%lu,%f,%f\r\n",burst_len, cl_write_clk_count_avg/CLK_FREQ*1000,
-				cl_write_clk_count_stdev/CLK_FREQ*1000);
-
 
 			burst_len *= 2;
 		}
@@ -143,7 +132,7 @@ int main(int argc, char ** argv)
 	free(buf1);
 	free(buf2);
 	delete fabricManager;
-	//delete pciHandler;
+	delete pciHandler;
 	delete dmaController;
 	delete stopwatch;
 
